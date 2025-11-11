ddf7b6794d8ae79ed268d9d243b6d7ada5b46439
munal777 <munalpoudel3@gmail.com>
2025-11-11 10:59:29 +0545

    completed all task

---
+++ Task 3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age <= 13)
+{
+    Console.WriteLine("You are Child.");
+}
+else if (age >13 && age <= 19)
+{
+    Console.WriteLine("You are Teenager.");
+}
+else
+{
+       Console.WriteLine("You are Adult.");
+}
---
+++ Task 4/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter any nuimber from 1-7: ");
+
+int num=Convert.ToInt32(Console.ReadLine());
+
+switch(num)
+{
+    case 1:
+    Console.WriteLine("Monday");
+    break;
+    case 2:
+    Console.WriteLine("Tuesday");
+    break;
+    case 3:
+    Console.WriteLine("Wednesday");
+    break;
+    case 4:
+    Console.WriteLine("Thursday");
+    break;
+    case 5:
+    Console.WriteLine("Friday");
+    break;
+    case 6:
+    Console.WriteLine("Saturday");
+    break;
+    case 7:
+    Console.WriteLine("Sunday");
+    break;
+    default:
+    Console.WriteLine("Invalid Input");
+    break;
+}
